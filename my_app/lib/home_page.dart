f6824a4a7e47672cb50864aae5eaefeed94058c9
celsosoares <celso.soares97c@gmail.com>
2022-12-04 17:55:30 -0300

    topico-3

--- my_app/lib/home_page.dart
+++ my_app/lib/home_page.dart
@@ -9,13 +9,12 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-
   void _pushSaved() {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
         builder: (context) {
           final tiles = _saved.map(
-            (pair){
+            (pair) {
               return ListTile(
                 title: Text(
                   pair.asPascalCase,
@@ -38,7 +37,7 @@ class _RandomWordsState extends State<RandomWords> {
             body: ListView(children: divided),
           );
         },
-      ), 
+      ),
     );
   }
 
@@ -71,73 +70,91 @@ class _RandomWordsState extends State<RandomWords> {
                 });
               }),
               tooltip:
-              cardMode ? 'List Vizualization' : 'Card Mode Vizualization',
+                  cardMode ? 'List Vizualization' : 'Card Mode Vizualization',
               icon: const Icon(Icons.auto_fix_normal_outlined),
             ),
           ],
         ),
-
         body: _buildSuggestions(cardMode));
   }
 
-    Widget _buildSuggestions(bool cardMode) {
-      if (cardMode == false) {
-        return ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemBuilder: (context, i) {
-            if (i.isOdd) return const Divider();
+  Widget _buildSuggestions(bool cardMode) {
+    if (cardMode == false) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
 
-            final index = i ~/ 2;
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(generateWordPairs().take(10));
-            }
-            return _buildRow(_suggestions[index], index);
-          },
-        );
-      } else {
-        return _cardVizualizaton();
-      }
-    }
-
-    Widget _buildRow(WordPair pair, int index) {
-      final alreadySaved = _saved.contains(_suggestions[
-      index]);
-      return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-          trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved ? const Color.fromARGB(223, 255, 7, 7) : null,
-              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'),
-          onTap: () {
-            setState(() {
-              if (alreadySaved) {
-                _saved.remove(_suggestions[index]);
-              } else {
-                _saved.add(_suggestions[index]);
-              }
-            });
-          });
-    }
-
-    Widget _cardVizualizaton() {
-      return GridView.builder(
-        padding: const EdgeInsets.all(12),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 2,
-            mainAxisSpacing: 2,
-            childAspectRatio: 8),
-        itemCount: _suggestions.length,
-        itemBuilder: (context, index) {
+          final index = i ~/ 2;
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
-          return Column(
-            children: [_buildRow(_suggestions[index], index)],
-          );
+          return _buildRow(_suggestions[index], index);
         },
       );
+    } else {
+      return _cardVizualizaton();
     }
   }
+
+  Widget _buildRow(WordPair pair, int index) {
+    final alreadySaved = _saved.contains(pair);
+
+    return Dismissible(
+      background: Container(
+        color: Colors.purple,
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _suggestions.remove(pair);
+        });
+      },
+      key: Key(pair.hashCode.toString()),
+      child: ListTile(
+        title: Text(pair.asPascalCase, style: _biggerFont),
+        trailing: IconButton(
+          icon:
+              alreadySaved ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+          color: alreadySaved ? Colors.red : null,
+          onPressed: () {
+            setState(() {
+              if (alreadySaved) {
+                Icon(
+                  Icons.favorite_border,
+                  color: null,
+                );
+                _saved.remove(pair);
+              } else {
+                Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                );
+                _saved.add(pair);
+              }
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _cardVizualizaton() {
+    return GridView.builder(
+      padding: const EdgeInsets.all(12),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
+          childAspectRatio: 8),
+      itemCount: _suggestions.length,
+      itemBuilder: (context, index) {
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return Column(
+          children: [_buildRow(_suggestions[index], index)],
+        );
+      },
+    );
+  }
+}
